c15aaf06f6507e99e9404e62b90475248e332913
ridelkombem <ridelkombem@yahoo.com>
2021-12-14 11:24:04 -0600

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,26 @@
-
-
 import 'package:flutter/material.dart';
 //The main function is the starting point for all flutter apps
-void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      backgroundColor: Colors.blue,
-      appBar: AppBar(
-        title:Center(child: Text('I Am Rich'),),
-            backgroundColor: Colors.lightBlue
-      ),
-      body:  Center(
-        child: Image(
-            image:AssetImage('images/diamond.png'),
+
+void main() => runApp( MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+            title: Center(
+              child: Text('I Am Rich'),
+            ),
+            backgroundColor: Colors.lightBlue),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
         ),
       ),
-    ),
-  ),
-  );
+    );
+  }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:i_am_rich/main.dart';
 
 void main() {
