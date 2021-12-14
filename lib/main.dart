56bba8d87c6a058aaa1f00c2731b252b09137586
ridelkombem <ridelkombem@yahoo.com>
2021-12-14 11:45:28 -0600

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 //The main function is the starting point for all flutter apps
 
-void main() => runApp( MyApp());
-
+void main() => runApp(
+      MyApp(),
+    );
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
